f62efdc1ff74b8f044ec5db1881416bcc910694f
nitijkatiyar <nitij>
2016-06-09 16:51:16 +0530

    Adding profile screen

--- Lyfe/app/src/main/java/com/bst/Lyfe/fragments/ProfileFragment.java
+++ Lyfe/app/src/main/java/com/bst/Lyfe/fragments/ProfileFragment.java
@@ -1,18 +1,44 @@
 package com.bst.Lyfe.fragments;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.bst.Lyfe.R;
+import com.commit451.nativestackblur.NativeStackBlur;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ProfileFragment extends Fragment {
 
     public ProfileFragment() {
     }
+
+    View view;
+
+    CircularImageView _profileImage;
+    ImageView _profileBackgroundImage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +49,114 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        _profileImage = (CircularImageView) view.findViewById(R.id.profile_image);
+        Bitmap defaultprofile = BitmapFactory.decodeResource(getResources(), R.mipmap.profile_default_male);
+        _profileImage.setImageBitmap(defaultprofile);
+
+        _profileBackgroundImage = (ImageView) view.findViewById(R.id.background_image);
+        Bitmap defaultprofileBackground = BitmapFactory.decodeResource(getResources(), R.mipmap.profile_default_male);
+        Bitmap bm = NativeStackBlur.process(defaultprofileBackground, 50);
+        _profileBackgroundImage.setImageBitmap(bm);
+
+        _profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        return view;
     }
 
+    private void selectImage() {
+
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    Bitmap bm = NativeStackBlur.process(bitmap, 50);
+                    _profileImage.setImageBitmap(bitmap);
+                    _profileBackgroundImage.setImageBitmap(bm);
+
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getActivity().getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("imagepath", "..." + picturePath);
+                Bitmap bm = NativeStackBlur.process(thumbnail, 50);
+                _profileImage.setImageBitmap(thumbnail);
+                _profileBackgroundImage.setImageBitmap(bm);
+            }
+        }
+    }
 }
