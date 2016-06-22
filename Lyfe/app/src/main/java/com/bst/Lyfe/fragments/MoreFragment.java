6667dfdeb300922dae1ab1f8f6c4c1cd3a51c162
Arun <arun.kawdiya89@gmail.com>
2016-06-22 17:05:47 +0530

    add blood facts resources

--- Lyfe/app/src/main/java/com/bst/Lyfe/fragments/MoreFragment.java
+++ Lyfe/app/src/main/java/com/bst/Lyfe/fragments/MoreFragment.java
@@ -1,15 +1,22 @@
 package com.bst.Lyfe.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.bst.Lyfe.R;
+import com.bst.Lyfe.activities.ActivityBloodFacts;
+import com.bst.Lyfe.activities.MainActivity;
 
 
 public class MoreFragment extends Fragment {
+    RelativeLayout _bloodFactsText;
+    MainActivity mainActivity;
+
 
     public MoreFragment() {
         // Required empty public constructor
@@ -23,8 +30,17 @@ public class MoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        mainActivity = (MainActivity) getActivity();
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+        _bloodFactsText=(RelativeLayout)view.findViewById(R.id.bloodFactsText);
+        _bloodFactsText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mainActivity, ActivityBloodFacts.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
