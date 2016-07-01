085cf75bbd6b260944e0f97a1e9dceae85baa29d
Arun <arun.kawdiya89@gmail.com>
2016-07-01 16:39:39 +0530

    commited more fragment code and hide keyboard code

--- Lyfe/app/src/main/java/com/bst/Lyfe/activities/ActivityBloodFacts.java
+++ Lyfe/app/src/main/java/com/bst/Lyfe/activities/ActivityBloodFacts.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.bst.Lyfe.R;
 import com.bst.Lyfe.adapter.BloodFactsAdapter;
@@ -18,7 +19,7 @@ import java.util.List;
  */
 public class ActivityBloodFacts extends AppCompatActivity {
 
-    String[] values = new String[]{"Blood is the life-maintaining fluid that circulates through the body's" +" "+
+    String[] values = new String[]{"Blood is the life-maintaining fluid that circulates through the body's" + " " +
             "heart, arteries, veins and capillaries.", "Blood carries to the body nourishment, electrolytes, hormones," +
             "vitamins, antibodies, heat, and oxygen.", "Blood carries away from the body waste matter and carbon dioxide.",
             "Blood fights against infection and helps heal wounds, keeping you healthy.",
@@ -28,8 +29,31 @@ public class ActivityBloodFacts extends AppCompatActivity {
             "blood.", "Red blood cells live about 120 days in the circulatory system. ", "Blood platelets help clotting and give those with leukemia and other\n" +
             "cancers a chance to live."};
 
+    String[] valuesmyth = new String[]{"Being a vegetarian, means that the blood does not have enough iron and cannot be donated.",
+            "Giving blood hurts.", "HIV or other infections can be contracted from donating blood." +
+            "Giving blood is time consuming.",
+            "There is limited blood in the body and it is unhealthy to give some away.",
+            "Age is a deterrent to blood donation.", "Heavy people are healthier and have more blood give.", "Health deteriorates after donating blood.",
+            "You cannot take part in sports or other physical activities after donating blood.",
+            "Taking medication means that one cannot be a blood donor.", "When there is a requirement, blood can be manufactured.",
+            "Being of mixed race precludes blood from being helpful.", "Blood donation can tell if one is HIV positive."};
+
+    String[] valuesFacts = new String[]{"Vegetarians can donate blood. The iron needed is taken from body stores and once a balanced diet is maintained is replaced after donation. This usually normally takes a month or so.",
+            "The pain experienced is no more than a needle prick. The slight soreness that maybe where the needle was is just a reminder of the good deed done.",
+            "A clear procedure exists for taking blood from each donor. Sterility is maintained at all steps. A sterile, new needle is used for each donation and is then properly discarded. Use of sterile equipment and technique limits the chance of infection.",
+            "The time taken for a single donation session is normally not more than an hour or so.", " Only about 350-450ml of blood is taken during a donation session. There is enough blood in the body to donate it without any ill effects. The body makes new blood after donation.",
+            "Anyone up to the age of 60 who is fit and healthy can give blood.", " Being overweight makes people less healthy. Overweight people do not have more blood.",
+            "If you are healthy prior to donation, your recovery is complete in a day or two. It is advised to rest a while after donating. Drinking enough liquids replaces the lost fluid within a couple of hours. The body produces new cells faster after a donation. All the RBCs are replaced within 3-4 days and WBCs within 3 weeks.",
+            "Giving blood does not interfere with ability to perform physically. Advice to avoid heavy lifting or strenuous workouts for the rest of the day is given after the donation. You can get back on track the next day.",
+            " Depending on the medication being taken, it may halt donation for a period, though in many cases it won't prevent a donation. person in charge or the nursing staff should be informed before donating.",
+            "Blood is not something that can be manufactured. It can only come from healthy human beings.", "Race and caste have no bearing on eligibility being a blood donor. It is the blood type and group that is of importance.", " HIV antibodies can take months to develop after infection with the virus. Those recently infected may have a negative test result and yet be able to infect others. It is better not to donate blood if at risk of getting HIV or other infections"};
+
     List<String> stringList = new ArrayList<String>(Arrays.asList(values));
+    List<String> stringListMyth = new ArrayList<String>(Arrays.asList(valuesmyth));
+    List<String> stringListFacts = new ArrayList<String>(Arrays.asList(valuesFacts));
     RecyclerView recyclerview;
+    public boolean aBoolean;
+    TextView headertext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,10 +61,22 @@ public class ActivityBloodFacts extends AppCompatActivity {
         setContentView(R.layout.activity_bloodfacts);
 
         recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
+        headertext=(TextView)findViewById(R.id.headertext);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerview.setLayoutManager(layoutManager);
         recyclerview.setBackgroundColor(getResources().getColor(R.color.colorTextLightBlack));
-        BloodFactsAdapter arrayAdapter = new BloodFactsAdapter(this, stringList);
+        Bundle bundle=getIntent().getExtras();
+        BloodFactsAdapter arrayAdapter = null;
+        if(bundle==null)
+        {
+            aBoolean=false;
+            arrayAdapter = new BloodFactsAdapter(this, stringList,aBoolean,"saaa");
+        }
+        else if(bundle.getString("myth").equals("myth")){
+            headertext.setText("Myths vs Facts about blood donation");
+            aBoolean=true;
+            arrayAdapter = new BloodFactsAdapter(this, stringListMyth,stringListFacts,aBoolean,"fsffks");
+        }
         recyclerview.setAdapter(arrayAdapter);
 
     }
--- Lyfe/app/src/main/java/com/bst/Lyfe/fragments/MoreFragment.java
+++ Lyfe/app/src/main/java/com/bst/Lyfe/fragments/MoreFragment.java
@@ -10,11 +10,14 @@ import android.widget.RelativeLayout;
 
 import com.bst.Lyfe.R;
 import com.bst.Lyfe.activities.ActivityBloodFacts;
+import com.bst.Lyfe.activities.BloodDonateActivity;
 import com.bst.Lyfe.activities.MainActivity;
+import com.bst.Lyfe.activities.SignInActivity;
+import com.bst.utils.ActivityChanger;
 
 
 public class MoreFragment extends Fragment {
-    RelativeLayout _bloodFactsText;
+    RelativeLayout _bloodFactsText,donateblood,myths;
     MainActivity mainActivity;
 
 
@@ -33,10 +36,33 @@ public class MoreFragment extends Fragment {
         mainActivity = (MainActivity) getActivity();
         View view = inflater.inflate(R.layout.fragment_more, container, false);
         _bloodFactsText=(RelativeLayout)view.findViewById(R.id.bloodFactsText);
+        donateblood=(RelativeLayout)view.findViewById(R.id.donateblood);
+        myths=(RelativeLayout)view.findViewById(R.id.myths);
+        donateblood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mainActivity, BloodDonateActivity.class);
+                startActivity(intent);
+
+            }
+        });
         _bloodFactsText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(mainActivity, ActivityBloodFacts.class);
+                startActivity(intent);
+            }
+        });
+        myths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                Intent intent = new Intent(mainActivity, ActivityBloodFacts.class);
+                Bundle mBundle = new Bundle();
+                mBundle.putString("myth", "myth");
+                intent.putExtras(mBundle);
                 startActivity(intent);
             }
         });
